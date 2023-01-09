cccf82ca90a22c0bd68962b38972a4476a476296
Kingyakstha <kingyakshrestha123@gmail.com>
2023-01-09 21:45:56 +0545

    Second

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const Goerli_RPC_URL = process.env.Goerli_RPC_URL
+const PRIVATE_key = process.env.PRIVATE_key
+
 module.exports = {
-  solidity: "0.8.17",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: Goerli_RPC_URL,
+            accounts: [PRIVATE_key],
+            chainId: 5,
+        },
+    },
+    solidity: "0.8.17",
+}
