d0042e471959aaafba3289da5a21e15b9e37afc4
Kingyakstha <kingyakshrestha123@gmail.com>
2023-01-14 15:36:02 +0545

    Commiting some of the new updates

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,14 @@ require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 require("@nomiclabs/hardhat-etherscan")
 require("./tasks/block_number")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const Goerli_RPC_URL = process.env.Goerli_RPC_URL
 const PRIVATE_key = process.env.PRIVATE_key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP = process.env.COINMARKETCAP
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -22,5 +25,12 @@ module.exports = {
         },
     },
     etherscan: { apiKey: ETHERSCAN_API_KEY },
+    gasReporter: {
+        enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP,
+    },
     solidity: "0.8.17",
 }
--- test/Test-deploy.js
+++ test/Test-deploy.js
@@ -12,9 +12,13 @@ describe("Simplestorage", function () {
     it("Should start with a favourite numer of 0", async function () {
         const currentValue = await simplestorage.retrieve()
         const expectedvalue = "0"
-
+        //assert
+        //expect
         assert.equal(currentValue.toString(), expectedvalue)
+        //expect(currentValue.toString().to.equal(expectedvalue))
     })
+    //it.only runs only this test
+    //or in terminal yarn hardhat test -grep any word used in it's description
     it("Should change the value to 23", async function () {
         const change = await simplestorage.store("23")
         const currentValue = await simplestorage.retrieve()
